bfad0d029bc401a268bdf7e25e6a0829b2098122
Gabriel Feitosa <gabrielmfeitosa07@gmail.com>
2026-01-31 21:57:48 -0300

    Project Jo-Ken-Po Kotlin

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
+fun main () {
+    val opcoes = listOf("Pedra", "Papel", "Tesoura")
 
-    val nome = "José Gabriel Monteiro Feitosa"
+    println("--- Jogo Iniciado ---")
+    println("Digite a sua escolha")
 
-    println(nome)
+    //Lendo a entrada do usuário
+    val jogadaUsuario = readln().replaceFirstChar { it.uppercase() } // Ajusta para letra inicial maiúscula
 
+    println("Usuário escolheu: $jogadaUsuario")
+
+    //O Computador escolhe um item aleatório da lista
+    val jogadaComputador = opcoes.random()
+
+    println("Computador escolheu: $jogadaComputador")
+
+    //Lógica de resultado (Apenas um rascunho por enquanto)
+    if (jogadaUsuario == jogadaComputador) {
+        println("Empate!")
+    } else {
+        println("Ainda vamos definir quem ganhou...")
+    }
 }
